bc65c91fa31093d31e05b1c54caa85240f0e31c9
vinicius Almeida <vinicius45anacleto@gmail.com>
2023-02-25 10:51:43 -0300

    main

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ st.write("paciente :{}".format(user_input))
 
 #dados de entrada
 
-X = df.drop(['Outcome'], axis=1)
+X = df.drop(['Outcome'], 1)
 Y = df['Outcome']
 
 #dados treinamento e teste
@@ -41,4 +41,42 @@ def get_user_data():
     blood_pressure = st.sidebar.slider("pressão sanguinea", 0, 122, 72)
     skin_thickness = st.sidebar.slider("Espessura da pele", 0, 99, 20)
     insulin = st.sidebar.slider("insulina", 0, 900, 30)
-    bmi = st.sidebar.slider("")
+    bmi = st.sidebar.slider("Indice de massa corporal", 0.0, 70.0, 15.0)
+    dpf = st.sidebar.slider("Historico familiar de diabetes", 0.0, 3.0, 0.0)
+    age = st.sidebar.slider("Idade", 15, 100, 21)
+
+    user_data = {"gravidez": pregnancies,
+             "glicose": glucose,
+             "Pressão sanguinea": blood_pressure,
+             "Espessura da pele": skin_thickness,
+             "Insulina": insulin,
+             "Indice massa corporal": bmi,
+             "Historico familiar de diabetes": dpf,
+             "Idade": age
+             }
+    #user_data = [pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, dpf, age]
+    features = pd.DataFrame(user_data, index = [0])
+    return features
+
+user_input_variables = get_user_data()
+
+#grafico
+
+st.bar_chart(user_input_variables)
+
+st.subheader("Dados do usuario")
+st.write(user_input_variables)
+
+dtc = DecisionTreeClassifier(criterion = 'entropy', max_depth = 3)
+dtc.fit(x_train, y_train)
+
+#acuracia do modelo
+st.subheader('Acurácia do modelo')
+st.write(accuracy_score(y_test, dtc.predict(x_test)) * 100)
+
+#previsão
+
+prediction = dtc.predict(user_input_variables)
+
+st.subheader("previsão: ")
+st.write(prediction)
